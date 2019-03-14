60e0dfc26afd6d0a340945af93775021dc972005
unknown <aurthurmusendame@gmail.com>
2019-03-14 11:10:47 +0200

    requrements update

--- dispatrace/settings.py
+++ dispatrace/settings.py
@@ -57,6 +57,8 @@ INSTALLED_APPS = [
     'fuel.apps.FuelConfig',
 
     'notify',
+    # 'django_archive',
+    'dbbackup', # https://django-dbbackup.readthedocs.io/en/stable/installation.html
 ]
 
 MIDDLEWARE = [
@@ -99,12 +101,12 @@ WSGI_APPLICATION = 'dispatrace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
@@ -117,6 +119,16 @@ DATABASES = {
 #     }
 # }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dispatrace',
+        'USER': 'aurthur',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
